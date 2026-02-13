3ec040d3f46754e67d09c1d479254caab48edbab
R.abhinaya <abhinaya094@gmail.com>
2026-02-13 13:14:24 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,21 +1,20 @@
-
+// db.js
 const mysql = require('mysql2');
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'maglev.proxy.rlwy.net',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'DtSwIQBGuKAkGJlhZpuFKarMzdbSTKdY',
-  database: process.env.DB_NAME || 'railway',
-  port: process.env.DB_PORT || 27507,
-});
 
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 
 connection.connect((err) => {
   if (err) {
     console.error('Database connection failed:', err);
-    process.exit(1); // stop server if DB connection fails
+    process.exit(1);
   }
   console.log('Connected to MySQL database!');
 });
 
 module.exports = connection;
-
